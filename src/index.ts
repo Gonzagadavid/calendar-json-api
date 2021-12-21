86b878b521f5b22c18247bf2a782377a5af3d56d
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-12-21 16:11:08 -0300

    adicionado cors, para tornar a api publica

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { error } from './middlewares';
 import routerRoot from './routers/root';
 
@@ -9,6 +10,8 @@ const { PORT } = process.env;
 const started = `started in port ${PORT}`;
 
 const app = express();
+
+app.use(cors());
 
 app.use('/', routerRoot);
 
