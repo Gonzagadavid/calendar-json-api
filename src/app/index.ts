a5a8cd840b86c8d2056b50cde8928fa94e8246ba
gonzagadavid <david.blesses@hotmail.com>
2022-04-02 22:36:45 -0300

    feat: desenvolvido documentação com swagger

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import { error } from '../middlewares';
 import routerRoot from '../routers/root';
+import swaggerDocs from '../documents/swagger.json';
 
 const app = express();
 
 app.use(cors());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/', routerRoot);
 
